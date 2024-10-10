2a1b926ceb1f6487d4c67f1f745adf57a4bb0a4c
Luiz Alves <115586109+luizz-dev@users.noreply.github.com>
2024-10-10 18:43:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirecionarL() {
+  window.location.href = "https://www.linkedin.com/in/luiz-alves-leite/";
+}
+function redirecionarG() {
+  window.location.href = "https://github.com/luizz-dev";
+}
+function redirecionarW() {
+  window.location.href = "https://wa.me/11958212008";
+}
+
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const mensagem = document.getElementById("mensagem");
+
+// contato
